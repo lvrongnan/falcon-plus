f84992568e1f480eaf6a32ac03b9c90a67045799
lvrn <lvrongnan@163.com>
2019-02-25 18:35:35 +0800

    (bug fix)获取核数返回值不能为int，应该为int64，否则transfer会过滤掉

--- modules/agent/funcs/cpustat.go
+++ modules/agent/funcs/cpustat.go
@@ -163,8 +163,8 @@ func CpuPrepared() bool {
 	defer psLock.RUnlock()
 	return procStatHistory[1] != nil
 }
-func CpuCores() uint64 {
-	return uint64(runtime.NumCPU())
+func CpuCores() int64 {
+	return int64(runtime.NumCPU())
 }
 
 func CpuMetrics() []*model.MetricValue {
